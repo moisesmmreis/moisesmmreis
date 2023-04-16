15e38518a7f6fb69f4b77e9373a0a354aba57a9f
Moisés Reis <118673344+moisesmmreis@users.noreply.github.com>
2023-04-16 20:17:44 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import react from "@astrojs/react";
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind(), react(), image()]
+  integrations: [tailwind(), react()]
 });
