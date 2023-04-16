c1f95e4af2b02971ecbd1981efbfa9c49a957da1
Moisés Reis <118673344+moisesmmreis@users.noreply.github.com>
2023-04-16 20:09:31 -0300

    Update Index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/edge';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), image()]
 });
