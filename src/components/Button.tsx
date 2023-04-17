6eb8e24ad44c03a5f08b8ae64b8464987f73415f
Moisés Reis <118673344+moisesmmreis@users.noreply.github.com>
2023-04-17 00:12:20 -0300

    Update index.astro

---
+++ src/components/Button.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { VariantProps, cva } from "class-variance-authority";
+
+import { cn } from "../lib/utils";
+
+const buttonVariants = cva("flex", {
+  variants: {
+    variant: {
+      default: "lowercase border px-2 py-1 rounded-xl w-fit",
+      link: "lowercase underline opacity-60 hover:opacity-100 transition-opacity cursor-pointer",
+    },
+    position: {
+      default: "relative",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    position: "default",
+  },
+});
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, position, ...props }, ref) => {
+    return (
+      <button
+        className={cn(buttonVariants({ variant, position, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
