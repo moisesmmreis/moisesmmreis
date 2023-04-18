1c2f4eaf45536dad34615f4d20c99d175e1eaa76
Moisés Reis <118673344+moisesmmreis@users.noreply.github.com>
2023-04-17 23:40:53 -0300

    Update Project.astro

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,8 +6,8 @@ import { cn } from "../lib/utils";
 const buttonVariants = cva("flex", {
   variants: {
     variant: {
-      default: "lowercase border px-2 py-1 rounded-xl w-fit",
-      link: "lowercase underline opacity-60 hover:opacity-100 transition-opacity cursor-pointer",
+      default: "text-base lowercase border px-2 py-1 rounded-xl w-fit",
+      link: "text-base lowercase underline opacity-60 hover:opacity-100 transition-opacity cursor-pointer",
     },
     position: {
       default: "relative",
