446a2587e867b90a602cd150002677cefc21e942
Moisés Reis <118673344+moisesmmreis@users.noreply.github.com>
2023-04-21 15:38:18 -0300

    Update Image.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vercelStatic from '@astrojs/vercel/static';
-;import tailwind from "@astrojs/tailwind";
+;
+import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: vercelStatic(),
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), image()]
 });
